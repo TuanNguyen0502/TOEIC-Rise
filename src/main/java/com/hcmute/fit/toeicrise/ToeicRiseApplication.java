2d297628f9ee41361df9025f7530d57bc7159cf0
Tuan <nguyenhahongtuan@gmail.com>
2025-09-07 22:43:30 +0700

    Initial commit

---
+++ src/main/java/com/hcmute/fit/toeicrise/ToeicRiseApplication.java
@@ -0,0 +1,13 @@
+package com.hcmute.fit.toeicrise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeicRiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToeicRiseApplication.class, args);
+    }
+
+}
