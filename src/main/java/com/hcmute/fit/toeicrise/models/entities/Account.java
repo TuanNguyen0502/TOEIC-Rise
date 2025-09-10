e6717cd113956e268fc13491bc49266a1c3975e6
Tuan <nguyenhahongtuan@gmail.com>
2025-09-10 17:35:58 +0700

    feat: Enhance Account entity with UserDetails implementation and add verification fields

--- src/main/java/com/hcmute/fit/toeicrise/models/entities/Account.java
+++ src/main/java/com/hcmute/fit/toeicrise/models/entities/Account.java
@@ -1,9 +1,13 @@
 package com.hcmute.fit.toeicrise.models.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "accounts")
@@ -12,7 +16,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Account extends BaseEntity {
+public class Account extends BaseEntity implements UserDetails {
     @Column(nullable = false, unique = true)
     private String email;
 
@@ -21,4 +25,47 @@ public class Account extends BaseEntity {
 
     @Column(columnDefinition = "VARCHAR(50)")
     private String authProvider;
+
+    @Column(name = "is_active", nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private Boolean isActive;
+
+    @Column(name = "verification_code", columnDefinition = "VARCHAR(255)")
+    private String verificationCode;
+
+    @Column(name = "verfication_code_expires_at")
+    private LocalDateTime verificationCodeExpiresAt;
+
+    @OneToOne(mappedBy = "account", cascade = CascadeType.ALL, optional = false)
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    //TODO: add proper boolean checks
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
 }
