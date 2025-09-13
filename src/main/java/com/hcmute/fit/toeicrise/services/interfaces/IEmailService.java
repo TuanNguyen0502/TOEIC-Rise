367a702cdbcd09434fb6168f74398b0e46874218
Tuan <nguyenhahongtuan@gmail.com>
2025-09-13 14:17:28 +0700

    feat: Enable asynchronous email sending for verification emails

--- src/main/java/com/hcmute/fit/toeicrise/services/interfaces/IEmailService.java
+++ src/main/java/com/hcmute/fit/toeicrise/services/interfaces/IEmailService.java
@@ -1,7 +1,9 @@
 package com.hcmute.fit.toeicrise.services.interfaces;
 
 import com.hcmute.fit.toeicrise.models.entities.Account;
+import org.springframework.scheduling.annotation.Async;
 
 public interface IEmailService {
+    @Async
     void sendVerificationEmail(Account account);
 }
