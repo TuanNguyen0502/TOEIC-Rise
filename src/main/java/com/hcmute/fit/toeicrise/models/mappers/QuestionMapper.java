f5d170576b67dabb3df8b282d31122033395d6f8
Tuan <nguyenhahongtuan@gmail.com>
2025-09-21 17:15:11 +0700

    feat: update QuestionMapper to include tag mapping in QuestionResponse

--- src/main/java/com/hcmute/fit/toeicrise/models/mappers/QuestionMapper.java
+++ src/main/java/com/hcmute/fit/toeicrise/models/mappers/QuestionMapper.java
@@ -3,9 +3,12 @@ package com.hcmute.fit.toeicrise.models.mappers;
 import com.hcmute.fit.toeicrise.commons.constants.Constant;
 import com.hcmute.fit.toeicrise.dtos.responses.QuestionResponse;
 import com.hcmute.fit.toeicrise.models.entities.Question;
+import com.hcmute.fit.toeicrise.models.entities.Tag;
 import org.mapstruct.*;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface QuestionMapper {
@@ -13,10 +16,16 @@ public interface QuestionMapper {
     @Mapping(source = "questionGroup.part.name", target = "part")
     @Mapping(source = "createdAt", target = "createdAt", dateFormat = Constant.DATE_TIME_PATTERN)
     @Mapping(source = "updatedAt", target = "updatedAt", dateFormat = Constant.DATE_TIME_PATTERN)
-    QuestionResponse toQuestionResponse(Question question, @Context List<String> tags);
+    @Mapping(source = "tags", target = "tags")
+    QuestionResponse toQuestionResponse(Question question);
 
-    @AfterMapping
-    default void setTags(@MappingTarget QuestionResponse dto, @Context List<String> tags) {
-        dto.setTags(tags);
+    default List<String> map(Set<Tag> tags) {
+        if (tags == null) {
+            return List.of();
+        }
+        return tags.stream()
+                .map(Tag::getName)
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
