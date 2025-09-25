c44685e9e7ed3e0b586b8a4ba08003deeea4c36e
Tuan <nguyenhahongtuan@gmail.com>
2025-09-25 21:46:20 +0700

    feat: add endpoint to retrieve current user information and implement user response mapping

---
+++ src/main/java/com/hcmute/fit/toeicrise/dtos/responses/CurrentUserResponse.java
@@ -0,0 +1,10 @@
+package com.hcmute.fit.toeicrise.dtos.responses;
+
+public class CurrentUserResponse {
+    private Long id;
+    private String fullName;
+    private String avatar;
+    private String email;
+    private String role;
+    private Boolean hasPassword;
+}
