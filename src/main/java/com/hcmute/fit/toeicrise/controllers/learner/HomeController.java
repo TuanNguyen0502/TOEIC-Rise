8384fefc0b118b51aee43f1000a68260694896ee
Tuan <nguyenhahongtuan@gmail.com>
2025-09-28 14:57:06 +0700

    feat: enhance authentication error handling

--- src/main/java/com/hcmute/fit/toeicrise/controllers/learner/HomeController.java
+++ src/main/java/com/hcmute/fit/toeicrise/controllers/learner/HomeController.java
@@ -1,13 +1,15 @@
-package com.hcmute.fit.toeicrise.controllers.learners;
+package com.hcmute.fit.toeicrise.controllers.learner;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("/learner/home")
+@RequestMapping("/learner")
+@RequiredArgsConstructor
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("")
     public String home() {
         return "Welcome to the Learner Home Page!";
     }
