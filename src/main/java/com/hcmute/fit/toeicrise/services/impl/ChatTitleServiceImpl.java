2706a1e1decbfbb9e0b2b90c4f30a05e32a53f6f
Tuan <nguyenhahongtuan@gmail.com>
2025-10-01 15:00:38 +0700

    feat: implement methods to rename and delete chat titles, and sort chat titles by updated date

--- src/main/java/com/hcmute/fit/toeicrise/services/impl/ChatTitleServiceImpl.java
+++ src/main/java/com/hcmute/fit/toeicrise/services/impl/ChatTitleServiceImpl.java
@@ -1,12 +1,16 @@
 package com.hcmute.fit.toeicrise.services.impl;
 
 import com.hcmute.fit.toeicrise.dtos.responses.ChatTitleResponse;
+import com.hcmute.fit.toeicrise.exceptions.AppException;
+import com.hcmute.fit.toeicrise.models.entities.ChatTitle;
+import com.hcmute.fit.toeicrise.models.enums.ErrorCode;
 import com.hcmute.fit.toeicrise.models.mappers.ChatTitleMapper;
 import com.hcmute.fit.toeicrise.repositories.ChatTitleRepository;
 import com.hcmute.fit.toeicrise.services.interfaces.IChatTitleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -15,10 +19,26 @@ public class ChatTitleServiceImpl implements IChatTitleService {
     private final ChatTitleRepository chatTitleRepository;
     private final ChatTitleMapper chatTitleMapper;
 
+    @Override
     public List<ChatTitleResponse> getAllChatTitlesByUserId(Long userId) {
         return chatTitleRepository.findAllByUser_Id(userId)
                 .stream()
+                .sorted(Comparator.comparing(ChatTitle::getUpdatedAt))
                 .map(chatTitleMapper::toChatTitleResponse)
                 .toList();
+    }
+
+    @Override
+    public void renameChatTitle(Long chatTitleId, String newTitle) {
+        ChatTitle chatTitle = chatTitleRepository.findById(chatTitleId)
+                .orElseThrow(() -> new AppException(ErrorCode.RESOURCE_NOT_FOUND, "Chat title"));
+        chatTitle.setTitle(newTitle);
+        chatTitleRepository.save(chatTitle);
+    }
+
+    public void deleteChatTitle(Long chatTitleId) {
+        ChatTitle chatTitle = chatTitleRepository.findById(chatTitleId)
+                .orElseThrow(() -> new AppException(ErrorCode.RESOURCE_NOT_FOUND, "Chat title"));
+        chatTitleRepository.delete(chatTitle);
     }
 }
--- src/main/java/com/hcmute/fit/toeicrise/services/interfaces/IChatTitleService.java
+++ src/main/java/com/hcmute/fit/toeicrise/services/interfaces/IChatTitleService.java
@@ -1,4 +1,11 @@
 package com.hcmute.fit.toeicrise.services.interfaces;
 
+import com.hcmute.fit.toeicrise.dtos.responses.ChatTitleResponse;
+
+import java.util.List;
+
 public interface IChatTitleService {
+    List<ChatTitleResponse> getAllChatTitlesByUserId(Long userId);
+
+    void renameChatTitle(Long chatTitleId, String newTitle);
 }
