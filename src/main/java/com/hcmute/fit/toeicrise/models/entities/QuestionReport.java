def32a9714d5d24f6726c667d02e65dfb77fe680
Tuan <nguyenhahongtuan@gmail.com>
2025-11-19 13:14:57 +0700

    feat: add question reporting functionality with reasons and status

---
+++ src/main/java/com/hcmute/fit/toeicrise/models/entities/QuestionReport.java
@@ -0,0 +1,43 @@
+package com.hcmute.fit.toeicrise.models.entities;
+
+import com.hcmute.fit.toeicrise.commons.utils.StringListJsonConverter;
+import com.hcmute.fit.toeicrise.models.enums.EQuestionReportReason;
+import com.hcmute.fit.toeicrise.models.enums.EQuestionReportStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "question_reports")
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionReport extends BaseEntity {
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @ManyToOne
+    @JoinColumn(name = "reporter_id", nullable = false)
+    private User reporter;
+
+    @ManyToOne
+    @JoinColumn(name = "resolver_id")
+    private User resolver;
+
+    @Convert(converter = StringListJsonConverter.class)
+    @Column(columnDefinition = "json")
+    private List<EQuestionReportReason> reasons;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private EQuestionReportStatus status;
+
+    @Column(name = "resolved_note", columnDefinition = "TEXT")
+    private String resolvedNote;
+}
