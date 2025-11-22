58800352f78e24d0a385953385e4affaa14372cf
Tuan <nguyenhahongtuan@gmail.com>
2025-11-23 02:47:17 +0700

    feat: enhance vector store service with document splitting for improved embedding

--- src/main/java/com/hcmute/fit/toeicrise/services/impl/VectorStoreServiceImpl.java
+++ src/main/java/com/hcmute/fit/toeicrise/services/impl/VectorStoreServiceImpl.java
@@ -4,6 +4,7 @@ import com.hcmute.fit.toeicrise.services.interfaces.ITestService;
 import com.hcmute.fit.toeicrise.services.interfaces.IVectorStoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
@@ -20,10 +21,20 @@ public class VectorStoreServiceImpl implements IVectorStoreService {
         // 1. Lấy danh sách Documents đã được format từ MySQL
         List<Document> documents = testService.loadTestsForVectorDB();
 
+        TokenTextSplitter splitter = new TokenTextSplitter(
+                1000, // chunk size (quan trọng nhất) - Đừng để quá 2048
+                400,  // min chunk size chars
+                10,   // min chunk length to embed
+                10000, // max num chunks
+                true   // keep separator
+        );
+
+        List<Document> splitDocuments = splitter.apply(documents);
+
         // 2. Lưu vào ChromaDB
         // Lưu ý: Nếu dữ liệu lớn, hãy chia nhỏ (batch) để tránh lỗi memory
-        vectorStore.add(documents);
+        vectorStore.add(splitDocuments);
 
-        System.out.println("Đã embed thành công " + documents.size() + " bài test.");
+        System.out.println("Đã embed thành công " + splitDocuments.size() + " bài test.");
     }
 }
