b6bfd8c373ea605f990b9cff0de5b5371e525ce1
HoangDuyen2 <thihoangduyendo@gmail.com>
2025-12-20 19:50:22 +0700

    fix dto

--- src/main/java/com/hcmute/fit/toeicrise/dtos/requests/question/QuestionGroupUpdateRequest.java
+++ src/main/java/com/hcmute/fit/toeicrise/dtos/requests/question/QuestionGroupUpdateRequest.java
@@ -5,13 +5,17 @@ import com.hcmute.fit.toeicrise.commons.constants.MessageConstant;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuestionGroupUpdateRequest {
     private MultipartFile audio;
 
--- src/main/java/com/hcmute/fit/toeicrise/dtos/requests/report/QuestionReportResolveRequest.java
+++ src/main/java/com/hcmute/fit/toeicrise/dtos/requests/report/QuestionReportResolveRequest.java
@@ -7,11 +7,15 @@ import com.hcmute.fit.toeicrise.models.enums.EQuestionReportStatus;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuestionReportResolveRequest {
     private EQuestionReportStatus status;
 
