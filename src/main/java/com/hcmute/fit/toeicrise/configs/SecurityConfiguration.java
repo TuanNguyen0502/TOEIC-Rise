303192612abb1fef2ae49921e1cacb4ce84996c0
HoangDuyen2 <thihoangduyendo@gmail.com>
2026-01-01 22:50:46 +0700

    add SecurityConfiguration

--- src/main/java/com/hcmute/fit/toeicrise/configs/SecurityConfiguration.java
+++ src/main/java/com/hcmute/fit/toeicrise/configs/SecurityConfiguration.java
@@ -75,7 +75,8 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://app-backend.com", "http://localhost:8080", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("\n" +
+                "https://toeic-rise.vercel.app", "http://localhost:8080", "http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
