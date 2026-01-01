d5f988533908c57745bf5e2762e70c7fbae065fa
HoangDuyen2 <thihoangduyendo@gmail.com>
2026-01-01 15:57:29 +0700

    change ToeicRiseApplicationTests

--- src/test/java/com/hcmute/fit/toeicrise/ToeicRiseApplicationTests.java
+++ src/test/java/com/hcmute/fit/toeicrise/ToeicRiseApplicationTests.java
@@ -1,13 +1,8 @@
 package com.hcmute.fit.toeicrise;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ToeicRiseApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
