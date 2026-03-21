b78ce155deff593f0344e3d82178f4e71fb16328
Tuan <nguyenhahongtuan@gmail.com>
2026-03-22 00:41:55 +0700

    feat: add BlogPostController with endpoint to retrieve blog posts by category for staff

---
+++ src/main/java/com/hcmute/fit/toeicrise/controllers/staff/BlogPostController.java
@@ -0,0 +1,25 @@
+package com.hcmute.fit.toeicrise.controllers.staff;
+
+import com.hcmute.fit.toeicrise.dtos.responses.PageResponse;
+import com.hcmute.fit.toeicrise.models.enums.EBlogPostStatus;
+import com.hcmute.fit.toeicrise.services.interfaces.IBlogPostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("staffBlogPostController")
+@RequestMapping("/staff/blog-posts")
+@RequiredArgsConstructor
+public class BlogPostController {
+    private final IBlogPostService blogPostService;
+
+    @GetMapping("/{category-slug}")
+    public ResponseEntity<PageResponse> getBlogPostsByCategoryForStaff(@PathVariable("category-slug") String categorySlug,
+                                                                       @RequestParam(required = false) String title,
+                                                                       @RequestParam(required = false) String slug,
+                                                                       @RequestParam(required = false) EBlogPostStatus status,
+                                                                       @RequestParam(defaultValue = "0") int page,
+                                                                       @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(blogPostService.getBlogPostsByCategoryForStaff(categorySlug, title, slug, status, page, size));
+    }
+}
