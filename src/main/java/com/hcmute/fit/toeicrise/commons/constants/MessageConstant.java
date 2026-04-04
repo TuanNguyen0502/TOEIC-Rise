846ef5d236dd5fa28b6eaedacbd68f7543222225
Tuan <nguyenhahongtuan@gmail.com>
2026-04-05 03:25:46 +0700

    feat: add controllers for speaking and writing test sets with CRUD operations

--- src/main/java/com/hcmute/fit/toeicrise/commons/constants/MessageConstant.java
+++ src/main/java/com/hcmute/fit/toeicrise/commons/constants/MessageConstant.java
@@ -5,7 +5,6 @@ public class MessageConstant {
     public static final String INVALID_PASSWORD = "Invalid password! Password must be 8-20 characters long, contain at least one digit, one lowercase letter, one uppercase letter, one special character from .@#$%^&+=, and have no whitespace.";
     public static final String INVALID_OTP = "Invalid OTP! OTP must be exactly 6 digits (0-9).";
     public static final String INVALID_TEST_SET = "Test set name can only contain letters, digits, spaces, and parentheses.";
-    public static final String TEST_SET_TYPE_NOT_NULL = "Test set type must not be null.";
     // Profile messages
     public static final String INVALID_USER = "Invalid or anonymous user";
     public static final String PROFILE_FULLNAME_NOT_NULL = "Full name must not be null.";
--- src/main/java/com/hcmute/fit/toeicrise/dtos/responses/testset/TestSetDetailResponse.java
+++ src/main/java/com/hcmute/fit/toeicrise/dtos/responses/testset/TestSetDetailResponse.java
@@ -2,7 +2,6 @@ package com.hcmute.fit.toeicrise.dtos.responses.testset;
 
 import com.hcmute.fit.toeicrise.dtos.responses.PageResponse;
 import com.hcmute.fit.toeicrise.models.enums.ETestSetStatus;
-import com.hcmute.fit.toeicrise.models.enums.ETestSetType;
 import lombok.Builder;
 import lombok.Data;
 
@@ -12,7 +11,6 @@ public class TestSetDetailResponse {
     private Long id;
     private String name;
     private ETestSetStatus status;
-    private ETestSetType type;
     private String createdAt;
     private String updatedAt;
     private PageResponse testResponses;
