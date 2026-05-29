45c209564d356d7201035a0e5b8336f85dd3f4c0
ldmvuong <minhvuongakira@gmail.com>
2026-05-29 21:23:04 +0700

    feat(security): add new allowed origin for CORS configuration

--- src/main/java/com/hcmute/fit/toeicrise/configs/SecurityConfiguration.java
+++ src/main/java/com/hcmute/fit/toeicrise/configs/SecurityConfiguration.java
@@ -85,7 +85,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://app-backend.com", "http://localhost:8080", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://app-backend.com", "http://localhost:8080", "http://localhost:5173", "https://toeic-rise-eight.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
